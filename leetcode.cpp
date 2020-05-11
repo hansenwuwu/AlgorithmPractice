af6b9d1a55aff37231d62e73705f075a63fa291d
Hs <hansenwu@micb2b.com>
2020-05-11 21:22:15 +0800

    20200511

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,6 +5,22 @@ using namespace std;
 int main(){
 
     printf("hello");
+
+    int n = 30;
+    int r = 0;
+
+    // n-1 times
+    for(int i=1; i<n; i++){
+        // 
+        for(int j=i+1; j<=n; j++){
+            // 
+            for(int k=1; k<=j; k++ ){
+                r++;
+            }
+        }
+    }
     
+    cout << r << endl;
+
     return 0;
 }
